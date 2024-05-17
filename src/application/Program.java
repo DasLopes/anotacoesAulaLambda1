caba1b7b268cf7cabed4642474de992ddf65b4ae
daslopes <daslopes@outlook.com.br>
2024-05-17 11:22:53 -0300

    Anonimous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,8 @@
 package application;
-import entities.MyComparetor;
 import entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -14,7 +14,14 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(new MyComparetor());
+        Comparator<Product> comp = new Comparator<Product>(){
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());        
+            }            
+        };
+
+        list.sort(comp);
 
         for (Product product : list) {
             System.out.println(product);
