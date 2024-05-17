7e0f706ff54a393cf9c6d04b4afde4934d648ab2
daslopes <daslopes@outlook.com.br>
2024-05-17 11:18:38 -0300

    Standart Comparator

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+import entities.MyComparetor;
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort(new MyComparetor());
+
+        for (Product product : list) {
+            System.out.println(product);
+        }
+    }
+}
